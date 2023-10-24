33307f80f5c54d38f86eb68ff1c589106c597f6f
akande56 <abdulsalamabubakar52@gmail.com>
2023-10-24 14:01:17 +0100

    migrate new database

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements/production.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-#echo "from django.contrib.auth import get_user_model; User= get_user_model(); User.objects.create_superuser('admin@gmail.com', 'abdul123.')"| python manage.py shell
+echo "from django.contrib.auth import get_user_model; User= get_user_model(); User.objects.create_superuser('admin@gmail.com', 'abdul123.')"| python manage.py shell
